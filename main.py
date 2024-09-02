906ae47191e21a28d08925522bc1c9cc485f2ac4
Petra Michael <petraxmichael@gmail.com>
2024-09-02 19:06:04 +0700

    v1.2 updated to upload results to github repo

--- main.py
+++ main.py
@@ -3,32 +3,20 @@ from bs4 import BeautifulSoup
 from datetime import datetime, timedelta
 import yfinance as yf
 import os
-import requests
 import pandas as pd
 from datetime import datetime
 import math as m
 from git import Repo
 from dotenv import load_dotenv
 
-def move_date(current_date):
-    url = f"https://sahamidx.com/?view=Home&date_now={current_date}&page=1"
-    response = requests.get(url)
 
-    soup = BeautifulSoup(response.text, 'html.parser')
-    h1_elements = soup.find_all('h1')
-    # print(current_date,h1_elements)
-    for h1 in h1_elements:
-        # print(h1.text)
-        if "Sorry, no data" in h1.text:
-            current_date = (datetime.strptime(current_date, '%Y-%m-%d') - timedelta(days=1)).strftime('%Y-%m-%d')
-            return 2, current_date
-        else:
-            return 1, current_date
+def load_stock_symbols(filename='stocklist.xlsx'):
+    df = pd.read_excel(filename, usecols=[1])  
+    stock_symbols = df.iloc[1:].dropna().squeeze().tolist()  
+    return stock_symbols
 
-    # Return a default value if no "Sorry, no data" is found
-    return 0, current_date
 
-# Function to extract data from the given URL
+all_values = load_stock_symbols()
 def scrape_data(url):
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
@@ -46,42 +34,18 @@ def scrape_data(url):
                     prev_value = current_value
     return values
 
-all_values = []
-check_dates = 2
-current_date = datetime.now().strftime('%Y-%m-%d')
 
-while check_dates == 2:
-    check_dates, current_date = move_date(current_date)
-    print(current_date)
-
-# Iterate pages
-for iter in range(1, 47):
-    url = f"https://sahamidx.com/?view=Home&date_now={current_date}&page={iter}"
-
-    # Scrape data from the current URL
-    values = scrape_data(url)
-    print(values)
-
-    # Add the values to the list
-    all_values.extend(values)
-
-# print(all_values)
+stocks = [item + ".JK" for item in all_values]
 stock = yf.Ticker("BBCA.JK")
 historical_data = stock.history(period='1d')
 last_entry_datetime = historical_data.index[-1].strftime("%Y-%m-%d")
 current_date = last_entry_datetime
 print(current_date)
-
-# Load environment variables from .env file
 load_dotenv()
-
-# Configuration from environment variables
 GITHUB_REPO = os.getenv('_GITHUB_REPO')
 GITHUB_TOKEN = os.getenv('_GITHUB_TOKEN')
 BRANCH_NAME = os.getenv('_BRANCH_NAME')
-
-# Create a temporary local repository for working with files
-TEMP_DIR = '/tmp/repo'
+TEMP_DIR = '/results'
 if not os.path.exists(TEMP_DIR):
     Repo.clone_from(f'https://github.com/{GITHUB_REPO}.git', TEMP_DIR, branch=BRANCH_NAME)
 repo = Repo(TEMP_DIR)
@@ -102,23 +66,16 @@ def create_excel_and_debug_files():
     current_date = datetime.now().strftime("%d-%m-%Y")
     excel_filename = f"stock_data_{current_date}.xlsx"
     debug_filename = f"debug_stock_scrapper_{current_date}.txt"
-
-    # Create empty files
     df_empty = pd.DataFrame()
     df_empty.to_excel(os.path.join(TEMP_DIR, excel_filename), index=False)
     
     with open(os.path.join(TEMP_DIR, debug_filename), 'w') as debug_file:
         debug_file.write("Debug Log - Stock Scrapper\n\n")
-
-    # Push files to GitHub
     push_to_github(excel_filename, '')
     push_to_github(debug_filename, '')
 
 create_excel_and_debug_files()
 
-# Your stock analysis code here...
-import yfinance as yf
-import math as m
 
 def get_perc_change(n_days, history_cls):
     temp_array = history_cls[:n_days]
@@ -199,7 +156,6 @@ header = [
 with pd.ExcelWriter(os.path.join(TEMP_DIR, excel_filename), engine='openpyxl') as writer:
     pd.DataFrame(columns=header).to_excel(writer, index=False, sheet_name='Sheet1')
 
-stocks = [item + ".JK" for item in all_values]
 
 for i, stock in enumerate(stocks, start=1):
     for period in ['max', 'ytd', '3mo', '1mo']:
@@ -238,89 +194,55 @@ for i, stock in enumerate(stocks, start=1):
             volume_const5 = analyze_bound_stock(5, history_cls, history_vol)
             volume_const20 = analyze_bound_stock(20, history_cls, history_vol)
             volume_const50 = analyze_bound_stock(50, history_cls, history_vol)
-            volume_analysis = (volume_const5 / 5 + volume_const20 / 20 + volume_const50 / 50) * ma_const
-            final_analysis = volume_analysis * info['marketCap'] / 1000000
 
-            stock_info = pd.DataFrame({
-                'Stock': [stock],
-                'Market Cap and Buy Analysis': [final_analysis],
-                'Buy Analysis': [volume_analysis],
-                'Volume Analysis Result': [volume_const5 + volume_const20 + volume_const50],
-                'Volume Analysis 5 Days': [volume_const5],
-                'Volume Analysis 20 Days': [volume_const20],
-                'Volume Analysis 50 Days': [volume_const50],
-                'MA Analysis': [ma_const],
-                'MA5': [ma5],
-                'MA5 Symbol': [display_ma[0]],
-                'MA10': [ma10],
-                'MA10 Symbol': [display_ma[1]],
-                'MA20': [ma20],
-                'MA20 Symbol': [display_ma[2]],
-                'MA50': [ma50],
-                'MA50 Symbol': [display_ma[3]],
-                'MA100': [ma100],
-                'MA100 Symbol': [display_ma[4]],
-                'MA200': [ma200],
-                'MA200 Symbol': [display_ma[5]],
-                '3D Change%': [perc3],
-                '5D Change%': [perc5],
-                '20D Change%': [perc20],
-                'Yesterday Closing Price': [history_cls[1]],
-                'Current Price': [history_cls[0]],
-                'Volatility in 3 Day': [vola3],
-                'Volatility in 5 Days': [vola5],
-                'Volatility in 20 Days': [vola20],
-                'Market Cap Value': [info['marketCap']],
-                'Volume Average in 5 Days': [vol5],
-                'Volume Average in 20 Days': [vol20],
-                'Volume Average in 50 Days': [vol50],
-                'Volume Average in 100 Days': [vol100],
-            })
+            market_cap = info.get('marketCap', 'N/A')
+            if market_cap != 'N/A':
+                market_cap = '{:.2f}T'.format(market_cap / 1e12) if market_cap > 1e12 else '{:.2f}B'.format(market_cap / 1e9)
 
-            # Save data to the local repository
-            with pd.ExcelWriter(os.path.join(TEMP_DIR, excel_filename), engine='openpyxl', mode='a', if_sheet_exists='overlay') as writer:
-                stock_info.to_excel(writer, index=False, sheet_name='Sheet1')
+            data = {
+                'Stock': stock,
+                'Market Cap and Buy Analysis': market_cap,
+                'Buy Analysis': ma_const,
+                'Volume Analysis Result': volume_const50,
+                'Volume Analysis 5 Days': volume_const5,
+                'Volume Analysis 20 Days': volume_const20,
+                'Volume Analysis 50 Days': volume_const50,
+                'MA Analysis': display_ma,
+                'MA5': ma5,
+                'MA5 Symbol': display_ma[0],
+                'MA10': ma10,
+                'MA10 Symbol': display_ma[1],
+                'MA20': ma20,
+                'MA20 Symbol': display_ma[2],
+                'MA50': ma50,
+                'MA50 Symbol': display_ma[3],
+                'MA100': ma100,
+                'MA100 Symbol': display_ma[4],
+                'MA200': ma200,
+                'MA200 Symbol': display_ma[5],
+                '3D Change%': perc3,
+                '5D Change%': perc5,
+                '20D Change%': perc20,
+                'Yesterday Closing Price': history_cls[1],
+                'Current Price': history_cls[0],
+                'Volatility in 3 Day': vola3,
+                'Volatility in 5 Days': vola5,
+                'Volatility in 20 Days': vola20,
+                'Market Cap Value': market_cap,
+                'Volume Average in 5 Days': vol5,
+                'Volume Average in 20 Days': vol20,
+                'Volume Average in 50 Days': vol50,
+                'Volume Average in 100 Days': vol100,
+            }
 
-            with open(os.path.join(TEMP_DIR, debug_filename), 'a') as debug_file:
-                debug_file.write(f"Successfully processed {stock} with period {period}.\n")
-            break
+            
+            with pd.ExcelWriter(os.path.join(TEMP_DIR, excel_filename), engine='openpyxl', mode='a') as writer:
+                pd.DataFrame([data]).to_excel(writer, index=False, sheet_name='Sheet1', header=False, startrow=i)
+
         except Exception as e:
             with open(os.path.join(TEMP_DIR, debug_filename), 'a') as debug_file:
                 debug_file.write(f"Error processing {stock} with period {period}: {str(e)}\n")
 
-    print("done:", i, "|", stock)
 
-print("#######all done#####")
-
-# Push the final Excel and debug files to GitHub
-with open(os.path.join(TEMP_DIR, excel_filename), 'rb') as f:
-    content = f.read()
-    response = requests.put(
-        f'https://api.github.com/repos/{GITHUB_REPO}/contents/{excel_filename}',
-        headers={
-            'Authorization': f'token {GITHUB_TOKEN}',
-            'Content-Type': 'application/vnd.github.v3+json'
-        },
-        json={
-            'message': 'Update stock data',
-            'content': content.encode('base64'),
-            'branch': BRANCH_NAME
-        }
-    )
-    response.raise_for_status()
-
-with open(os.path.join(TEMP_DIR, debug_filename), 'rb') as f:
-    content = f.read()
-    response = requests.put(
-        f'https://api.github.com/repos/{GITHUB_REPO}/contents/{debug_filename}',
-        headers={
-            'Authorization': f'token {GITHUB_TOKEN}',
-            'Content-Type': 'application/vnd.github.v3+json'
-        },
-        json={
-            'message': 'Update debug log',
-            'content': content.encode('base64'),
-            'branch': BRANCH_NAME
-        }
-    )
-    response.raise_for_status()
+push_to_github(excel_filename, '')
+push_to_github(debug_filename, '')
